17c7cb3284fbdfa377312d40f57a527d68187640
hossain <hossainahamedk@gmail.com>
2024-05-12 15:22:20 +0600

    completed initial deployment

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
     origin: [
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://save-food-ba204.web.app',
+        'https://save-food-ba204.firebaseapp.com',
     ],
     credentials: true
 }));
@@ -27,10 +29,16 @@ const client = new MongoClient(uri, {
     }
 });
 
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const foodCollection = client.db('saveFoodDB').collection('foods');
 
@@ -39,17 +47,17 @@ async function run() {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'none'
-            })
-            res.send({ token })
+            res
+                .cookie('token', token, cookieOptions)
+                .send({ success: true })
         })
 
         app.post('/logout', async (req, res) => {
             const user = req.body;
-            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+
+            res
+                .clearCookie('token', { ...cookieOptions, maxAge: 0 })
+                .send({ success: true })
         })
 
         // food related api 
@@ -62,7 +70,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
